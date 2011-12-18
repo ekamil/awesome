04f7dfd82654272baaf83ff462b8e11aba661b43
Kamil Essekkat <kamil@essekkat.pl>
2011-12-18 16:13:27 +0100

    Some widgets from https://github.com/tony/awesome-config

--- .config/awesome/widgets.lua
+++ .config/awesome/widgets.lua
@@ -7,16 +7,69 @@ vicious.register(mpdwidget, vicious.widgets.mpd,
         else 
             return args["{Artist}"]..' - '.. args["{Title}"]
         end
-    end, 10)
+    end, 5)
 -- }}}
--- {{{ battery
-batwidget = awful.widget.progressbar()
-batwidget:set_width(8)
-batwidget:set_height(10)
-batwidget:set_vertical(true)
-batwidget:set_background_color("#494B4F")
-batwidget:set_border_color(nil)
-batwidget:set_color("#AECF96")
-batwidget:set_gradient_colors({ "#AECF96", "#88A175", "#FF5656" })
-vicious.register(batwidget, vicious.widgets.bat, "$2", 61, "BAT1")
+
+-- {{{ Reusable separator
+separator = widget({ type = "imagebox" })
+separator.image = image(beautiful.widget_sep)
+
+spacer = widget({ type = "textbox" })
+spacer.width = 3
 -- }}}
+
+-- {{{ Date and time
+dateicon = widget({ type = "imagebox" })
+dateicon.image = image(beautiful.widget_date)
+-- Initialize widget
+datewidget = widget({ type = "textbox" })
+-- Register widget
+vicious.register(datewidget, vicious.widgets.date, date_format, 61)
+-- }}}
+
+-- {{{ Volume level
+volicon = widget({ type = "imagebox" })
+volicon.image = image(beautiful.widget_vol)
+-- Initialize widgets
+volbar = awful.widget.progressbar()
+volwidget = widget({ type = "textbox" })
+-- Progressbar properties
+volbar:set_vertical(true):set_ticks(true)
+volbar:set_height(16):set_width(8):set_ticks_size(2)
+volbar:set_background_color(beautiful.fg_off_widget)
+volbar:set_gradient_colors({ beautiful.fg_widget,
+   beautiful.fg_center_widget, beautiful.fg_end_widget
+}) -- Enable caching
+vicious.cache(vicious.widgets.volume)
+-- Register widgets
+vicious.register(volbar, vicious.widgets.volume, "$1", 2, "PCM")
+vicious.register(volwidget, vicious.widgets.volume, " $1%", 2, "PCM")
+-- Register buttons
+volbar.widget:buttons(awful.util.table.join(
+   awful.button({ }, 1, function () exec("kmix") end),
+   awful.button({ }, 4, function () exec("amixer -q set PCM 2dB+", false) vicious.force({volbar, volwidget}) end),
+   awful.button({ }, 5, function () exec("amixer -q set PCM 2dB-", false) vicious.force({volbar, volwidget}) end)
+)) -- Register assigned buttons
+volwidget:buttons(volbar.widget:buttons())
+-- }}}
+
+
+-- {{{ Battery state
+
+-- Initialize widget
+batwidget = widget({ type = "textbox" })
+baticon = widget({ type = "imagebox" })
+
+-- Register widget
+vicious.register(batwidget, vicious.widgets.bat,
+function (widget, args)
+if args[2] == 0 then return ""
+else
+baticon.image = image(beautiful.widget_bat)
+return "<span color='white'>".. args[2] .. "%</span>"
+end
+end, 61, "BAT0"
+)
+-- }}}
+
+
