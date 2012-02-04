3d8d026a88562cfc9e0496d4b11aaead9cbec73d
Kamil Essekkat <kamil@essekkat.pl>
2012-02-04 16:16:34 +0100

    Rules in separate file.

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -1,17 +1,20 @@
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-     { rule = { class = "Gvim" },
-      properties = { size_hints_honor = false } },
+window_rules = {
+-- web
+{ match = { "Firefox" }, tag = "2", },
+-- Pidgin
+{ match = { "Pidgin" }, tag = "3", float=true,  },
+{ match = { "buddy_list" }, tag = "3", float = false, nofocus = true, geometry = {0,16,350,736}},
+-- gimp
+{ match = { "Gimp" }, tag = "6", },
+{ match = { "gimp%-image%-window" }, geometry = {175,15,930,770}, border_width = 0 },
+{ match = { "^gimp%-toolbox$" }, geometry = {0,15,175,770}, slave = true, border_width = 0 },
+{ match = { "^gimp%-dock$" }, geometry = {1105,15,175,770}, slave = true, border_width = 0 },
+-- x-video
+{ match = { "MPlayer" }, geometry = {0,15,nil,nil}, float = true },
+-- chromium
+{ match = { "Chromium" }, tag = "5"},
+-- eclipse
+{ match = { "Eclipse" }, tag = "4" },
+
+{ match = { "Dia" }, float=true,  }
 }
--- .config/awesome/tags.lua
+++ .config/awesome/tags.lua
@@ -1,4 +1,5 @@
 require("shifty")
+require("rules")
 
 -- shifty: predefined tags
 shifty.config.tags = {
@@ -14,25 +15,7 @@ shifty.config.tags = {
 
 
 -- shifty: tags matching and client rules
-shifty.config.apps = {
--- web
-{ match = { "Firefox" }, tag = "2", },
--- Pidgin
-{ match = { "Pidgin" }, tag = "3", float=true,  },
-{ match = { "buddy_list" }, tag = "3", float = false, nofocus = true, geometry = {0,16,350,736}},
--- gimp
-{ match = { "Gimp" }, tag = "6", },
-{ match = { "gimp%-image%-window" }, geometry = {175,15,930,770}, border_width = 0 },
-{ match = { "^gimp%-toolbox$" }, geometry = {0,15,175,770}, slave = true, border_width = 0 },
-{ match = { "^gimp%-dock$" }, geometry = {1105,15,175,770}, slave = true, border_width = 0 },
--- x-video
-{ match = { "MPlayer" }, geometry = {0,15,nil,nil}, float = true },
--- chromium
-{ match = { "Chromium" }, tag = "5"},
--- eclipse
-{ match = { "Eclipse" }, tag = "4" },
-
-
+shifty.config.apps = {window_rules,
 -- client manipulation
 { match = { "" },
 honorsizehints = false,
