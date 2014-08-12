30daa0fd42bffdc914279283d9bcd75fc8cdc34b
Kamil Essekkat <kamil@essekkat.pl>
2014-08-12 17:29:01 +0200

    Panel on top after all

--- .config/awesome/config.lua
+++ .config/awesome/config.lua
@@ -5,7 +5,7 @@ local shell = os.getenv("SHELL")
 config.terminal = "xfce4-terminal -e " .. shell
 config.alt_terminal = config.userhome .. "/.local/bin/urxvt-zenburn.sh"
 config.modkey = "Mod4"
-config.panel_position = "bottom"
+config.panel_position = "top"
 
 local naughty = require("naughty")
 
@@ -15,7 +15,7 @@ naughty.config.default_preset.timeout = 10
 -- naughty.config.default_preset.ontop
 -- naughty.config.default_preset.margin
 -- naughty.config.default_preset.border_width
-naughty.config.default_preset.position = "bottom_right"
+naughty.config.default_preset.position = config.panel_position .. "_right"
 
 
 return config
