53dc292cf225190762ea0c7a4a3dec097901876c
Kamil Essekkat <kamil@essekkat.pl>
2014-08-14 14:11:57 +0200

    Typo

--- .config/awesome/config.lua
+++ .config/awesome/config.lua
@@ -3,7 +3,7 @@ local config = {}
 config.userhome = os.getenv("HOME")
 local shell = os.getenv("SHELL")
 config.terminal = "xfce4-terminal -e " .. shell
-config.alt_terminal = config.userhome .. "/.local/bin/ur135xvt-zenburn.sh"
+config.alt_terminal = config.userhome .. "/.local/bin/urxvt-zenburn.sh"
 config.modkey = "Mod4"
 config.panel_position = "top"
 
