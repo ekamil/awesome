50349df185bf1ccc0ebf77be083036a298adb5ce
Kamil Essekkat <kamil@essekkat.pl>
2014-12-03 10:13:39 +0100

    different outputs

--- .config/awesome/layouts/only_vga.sh
+++ .config/awesome/layouts/only_vga.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off \
+xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off \
        --output eDP1 --off \
-       --output VGA1 --mode 1920x1200 --pos 0x0 --rotate normal --primary
+       --output DP2 --mode 1920x1200 --pos 0x0 --rotate normal --primary
